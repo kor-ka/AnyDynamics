d9266cc5238338c93c8aede0cd27ab94db8f3627
kor-ka <badgrib@gmail.com>
2013-09-19 16:01:48 +0400

    Update DbTool.java
    fix INNER JOIN -> table, table WHERE ...

--- src/ru/kor_inc/andy/DbTool.java
+++ src/ru/kor_inc/andy/DbTool.java
@@ -127,7 +127,7 @@ public class DbTool{
      		dateFrom.setDate(dateFrom.getDate()+1);
       	} while(dateFrom.getDate() < dateTo.getDate());
      	
-        Cursor c = db.query(currentTable + " INNER JOIN dateFix ON " +currentTable+".date=dateFix.date "+currentTable+".numeric=dataFix.numeric" , new String[] {groupBy, "sum("+columnToSum+") as "+columnToSum }, null, null, groupBy, null, columnToSum+" DESC");
+        Cursor c = db.query(currentTable + " ,dateFix WHERE " +currentTable+".date=dateFix.date AND "+currentTable+".numeric=dataFix.numeric" , new String[] {groupBy, "sum("+columnToSum+") as "+columnToSum }, null, null, groupBy, null, columnToSum+" DESC");
      	
         return c;
          
